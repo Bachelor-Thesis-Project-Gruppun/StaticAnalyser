aaf670e78ff1b8bab1b6a3a20210e519809a8493
Vidar Magnusson <git@vidarmagnusson.com>
2020-02-10 20:39:09 +0100

    Fixed PMD warnings v0.0.0.0.1

---
+++ src/main/java/base/MainProgram.java
@@ -0,0 +1,11 @@
+package base;
+
+public final class MainProgram {
+    private MainProgram() {
+        System.out.println("No idea");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
