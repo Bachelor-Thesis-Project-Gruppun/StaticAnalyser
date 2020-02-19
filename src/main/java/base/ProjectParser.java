65610411db41767a24047a0e681e77feb2dfbe55
JonazIT <hogne@student.chalmers.se>
2020-02-19 17:27:26 +0100

    First very rough draft

---
+++ src/main/java/base/ProjectParser.java
@@ -0,0 +1,75 @@
+package base;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.SourceRoot;
+
+@Nonnull
+public class ProjectParser {
+    private String sourceRoot;
+    private List<CompilationUnit> compilationUnits;
+
+    public ProjectParser(String sourceRoot){
+        this.sourceRoot = sourceRoot;
+
+        compilationUnits = projectToAST();
+    }
+
+    private List<CompilationUnit> projectToAST() {
+        Path pathToSource = Paths.get(sourceRoot);
+        SourceRoot sourceRoot = new SourceRoot(pathToSource);
+        try {
+            sourceRoot.tryToParse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sourceRoot.getCompilationUnits();
+    }
+
+    public void verify() {
+        for(CompilationUnit cu : compilationUnits) {
+            cu.accept(new AnnotationGetter(), null);
+        }
+    }
+
+    private static class AnnotationGetter extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(
+            MarkerAnnotationExpr annotationExpr,
+            Void list) {
+            super.visit(annotationExpr, list);
+
+            // All of this is very rough, but it is something to start with
+            if(isCustomAnnotation(annotationExpr)) {
+                CompilationUnit cu =
+                    annotationExpr.findRootNode().findCompilationUnit().get();
+                System.out.println("Class: " + cu.getStorage().get().getFileName() +
+                                   "\nTested patterns:\n" + annotationExpr.getNameAsString() +
+                                   ": " +
+                                   PatternVerifyer.verifyPattern(annotationExpr.getNameAsString(), cu));
+                // If we can get the proper enum from the annotation that would
+                // be nicer
+            }
+
+        }
+
+        private boolean isCustomAnnotation(AnnotationExpr ann) {
+            return ann.getNameAsString().equals("Singleton") ||
+                   ann.getNameAsString().equals("Immutable");
+        }
+    }
+
+}
+
+
---
+++ src/main/java/base/Test.java
@@ -0,0 +1,10 @@
+package base;
+
+import groovy.lang.Singleton;
+
+@Singleton
+public class Test {
+    public Test() {
+        System.out.println("Hello");
+    }
+}
