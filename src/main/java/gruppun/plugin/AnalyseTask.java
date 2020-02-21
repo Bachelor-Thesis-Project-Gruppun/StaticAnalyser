0093ea3519dc507cd788d848248009c7f192741b
Vidar Magnusson <git@vidarmagnusson.com>
2020-02-21 19:35:03 +0100

    Add documentation and prepared for merge with base code

--- src/main/java/gruppun/plugin/AnalyseTask.java
+++ src/main/java/gruppun/plugin/AnalyseTask.java
@@ -1,12 +1,43 @@
 package gruppun.plugin;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskAction;
 
+/**
+ * A gradle task that will check verify classes against design patterns as
+ * described by certain annotations.
+ */
 public class AnalyseTask extends DefaultTask {
     @TaskAction
     public void check() throws GradleException {
-        System.out.println("TEST");
+        Project project = this.getProject();
+        List<Path> paths = new ArrayList<>();
+
+        SourceSet mainSourceSet = project.getConvention().getPlugin(
+            JavaPluginConvention.class).getSourceSets().getByName("main");
+
+
+        mainSourceSet.getAllJava().getSrcDirs().forEach(file -> {
+            paths.add(Paths.get(file.getAbsolutePath()));
+        });
+
+        paths.forEach(path -> {
+            // The path we want to pass to JavaParser
+            System.out.println("Path:: " + path.toString());
+        });
+
+        if (false) {
+            // Example on how we can fail a build.
+            throw new GradleException("Class A does not fullfill pattern B!");
+        }
     }
 }
--- src/main/java/gruppun/plugin/AnalyserPlugin.java
+++ src/main/java/gruppun/plugin/AnalyserPlugin.java
@@ -3,6 +3,10 @@ package gruppun.plugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+/**
+ * A gradle plugin -- Responsible for creating the Analyse task and making it
+ * a dependency for the check task.
+ */
 public class AnalyserPlugin implements Plugin<Project> {
      @Override
         public void apply(Project project) {
