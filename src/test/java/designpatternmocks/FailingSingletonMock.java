b7e361c902c31279e3aee80cbcd57d020559d16e
Darkarca <erik.gunnarsson98@gmail.com>
2020-02-28 13:41:00 +0100

    Created a testmock aswell as a failing testmock for the SingletonPattern, the failing mock should fail all predicates and the SingletonMock class should pass all predicates upon testing

---
+++ src/test/java/designpatternmocks/FailingSingletonMock.java
@@ -0,0 +1,15 @@
+package designpatternmocks;
+
+/**
+ * Mock class used to make sure that the SingletonVerifier does not return false positives for the SingletonPattern
+ * Should fail all predicates
+ */
+//@DesignPattern(pattern={Pattern.SINGLETON})
+public class FailingSingletonMock {
+    public static FailingSingletonMock failMock;
+
+    public FailingSingletonMock getInstance(){
+        return failMock;
+    }
+
+}
---
+++ src/test/java/designpatternmocks/SingletonMock.java
@@ -0,0 +1,37 @@
+package designpatternmocks;
+
+/**
+ * Mock class for the SingletonPattern, contains a private static field of SingletonMock, a private constructor aswell as a
+ * private static method which calls the constructor. the getInstance method is called from a public static method.
+ */
+//@DesignPattern(pattern={Pattern.SINGLETON})
+public class SingletonMock {
+    //  Private Static field of SingletonMock, should pass first predicate
+    private static SingletonMock instance;
+
+    /**
+     * Private constructor, should pass second predicate
+     */
+    private SingletonMock(){
+        System.out.println("Constructor called");
+    }
+
+    /**
+     * private static method getInstance which calls the constructor, should pass third predicate if called
+     * @return The Singleton instance of SingletonMock
+     */
+    private static SingletonMock getInstance(){
+        if(instance == null){
+            instance = new SingletonMock();
+        }
+        return instance;
+    }
+
+    /**
+     * Calls getInstance, should help passing the third predicate
+     * @return The Singleton instance of SingletonMock
+     */
+    public static SingletonMock callGetInstance(){
+        return getInstance();
+    }
+}
