ac3a7abe981c83401dee1f89ba130445798edc4c
Darkarca <erik.gunnarsson98@gmail.com>
2020-03-05 12:43:06 +0100

    Initial commit, created verifier class

---
+++ src/main/java/patternverifiers/DecoratorVerifier.java
@@ -0,0 +1,21 @@
+package patternverifiers;
+
+import com.github.javaparser.ast.CompilationUnit;
+
+/**
+ * A verifier for the decorator pattern
+ */
+public class DecoratorVerifier implements IPatternVerifier {
+
+    /**
+     * Verifies that every class in the CompilationUnit is part of the Decorator pattern
+     *
+     * @param compUnit The CompilationUnit of files to look at
+     *
+     * @return True if all classes in the CompilationUnit is part of the Decorator design pattern
+     */
+    @Override
+    public boolean verify(CompilationUnit compUnit) {
+        return false;
+    }
+}
