0c7f52895cb93cfc8e47ad404bb4d9100f6276eb
Li <li.ronning@gmail.com>
2020-03-06 18:22:08 +0100

    Change adaperVerifier according to ow the new framework likely will be

--- src/main/java/patternverifiers/AdapterVerifier.java
+++ src/main/java/patternverifiers/AdapterVerifier.java
@@ -1,18 +1,27 @@
 package patternverifiers;
 
+import java.util.List;
+import java.util.Map;
+
 import com.github.javaparser.ast.CompilationUnit;
+
+import base.Pattern;
 
 /**
  * A verifier for the adapter pattern.
  */
-public class AdapterVerifier implements IPatternVerifier{
+public class AdapterVerifier /* REMEBER TO IMPLEMENT THE INTERFACE */{
 
     public AdapterVerifier() {
     }
 
-    @Override
-    public boolean verify(CompilationUnit compUnit) {
-        throw new UnsupportedOperationException();
+    public boolean verify(Map<Pattern, List<CompilationUnit>> patternParts) {
+
+        // Identify the parts that belong together
+
+        // Verify if the parts are a coherent pattern
+
+        return false;
     }
 
 }
