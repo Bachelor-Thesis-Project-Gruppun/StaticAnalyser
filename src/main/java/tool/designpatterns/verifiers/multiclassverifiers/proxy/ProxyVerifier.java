2af444b775665d1e662e9e3f236f136acaeed8a3
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-03-26 15:33:03 +0100

    Divided work.
    Co-Authored-By: lir108 <lir108@users.noreply.github.com>

--- src/main/java/tool/designpatterns/verifiers/multiclassverifiers/proxy/ProxyVerifier.java
+++ src/main/java/tool/designpatterns/verifiers/multiclassverifiers/proxy/ProxyVerifier.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 
+import groovy.lang.Tuple2;
+import org.apache.commons.lang.NotImplementedException;
 import tool.designpatterns.Pattern;
 import tool.designpatterns.PatternGroup;
 import tool.designpatterns.verifiers.IPatternGrouper;
@@ -19,13 +22,46 @@ public class ProxyVerifier implements IPatternGrouper {
         Map<Pattern, List<ClassOrInterfaceDeclaration>> map) {
 
         List<Feedback> feedbacks = new ArrayList<>();
-        // Gå igenom allt:
-        // 1. Hitta en interface / abstrakt klass       --
-        // 2. Kolla om denna har en (abstrakt) metod.   --
-        // 3. Hitta 2 klasser som implementerar denna metod.  --
-        // 4. Kolla om en av dessa (klasser) har en variable av typ den andra. --
-        // 5. Kolla om samma (klass) kallar på den givna metoden i den andra klassen. --
+        // 1. Gå igenom alla "Pattern.PROXY_INTERFACE":
+        //    1b. hämta giltiga metoder.
+        // 2. För alla giltiga metoder:
+        //    2b. Gå igenom alla "Pattern.PROXY_SUBJECT":
+        //        2c. Kolla att den implementerar metoden (/ inhertetar från interfacet).
+        //        2d. Gå igenom alla Pattern.PROXY_PROXY"
+        //            2e. Kolla att den också har metoden (/ inhertetar från interfacet).
+        //            2f. Kolla att den har en privat variable av typ SUBJECT.
+        //            2g. Kolla att SUBJECTs metod kallas i metoden.
 
         return new PatternGroupFeedback(PatternGroup.PROXY, feedbacks);
+    }
+
+    // Steps 1 / 1b -- nemo
+    private Tuple2<Feedback, List<MethodDeclaration>> getValidMethods(
+        ClassOrInterfaceDeclaration classOrI) {
+
+        throw new NotImplementedException();
+    }
+
+    // Step 2 / 2b (also 2* but uses verifyProxy && classImplementsMethod for this) -- vidde
+    private Feedback verifyImplementors(
+        Map<ClassOrInterfaceDeclaration, List<MethodDeclaration>> interfaceMethods,
+        List<ClassOrInterfaceDeclaration> subjects, List<ClassOrInterfaceDeclaration> proxys) {
+
+        throw new NotImplementedException();
+    }
+
+    // 2d / 2f / 2g (also 2e but uses ClassImplementsMethod) -- vidde
+    private Feedback verifyProxy(
+        List<ClassOrInterfaceDeclaration> proxys, MethodDeclaration method,
+        ClassOrInterfaceDeclaration subject, MethodDeclaration subjectMethod) {
+
+        throw new NotImplementedException();
+    }
+
+    // Steps 2c / 2e -- nemo
+    private Feedback classImplementsMethod(
+        ClassOrInterfaceDeclaration theClass, ClassOrInterfaceDeclaration theInterface,
+        MethodDeclaration method) {
+        throw new NotImplementedException();
     }
 }
