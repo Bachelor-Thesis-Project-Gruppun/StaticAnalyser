4b1827a86af40edc8cd5661cc87516862819e975
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-03-27 14:25:37 +0100

    Add feedbackWrapper.

---
+++ src/main/java/tool/feedback/FeedbackWrapper.java
@@ -0,0 +1,21 @@
+package tool.feedback;
+
+public class FeedbackWrapper<T> {
+
+    private Feedback feedback;
+    private T other;
+
+    public FeedbackWrapper(Feedback feedback, T other) {
+        this.feedback = feedback;
+        this.other = other;
+    }
+
+    public Feedback getFeedback() {
+        return feedback;
+    }
+
+    public T getOther() {
+        return other;
+    }
+
+}
