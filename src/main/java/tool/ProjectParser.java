9178d099854dcc7ef4f2c5bc7a845cdf83075a10
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-01 01:38:31 +0200

    Now actually able to use symbol solver.

--- src/main/java/tool/ProjectParser.java
+++ src/main/java/tool/ProjectParser.java
@@ -7,11 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.utils.SourceRoot;
 
 import tool.designpatterns.DesignPattern;
@@ -35,11 +31,7 @@ public final class ProjectParser {
      */
     public static List<ClassOrInterfaceDeclaration> findAllClassesAndInterfaces(String sourcePath) {
         // Enable symbolsolving.
-        TypeSolver typeSolver = new CombinedTypeSolver();
-        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
-        ParserConfiguration config = StaticJavaParser.getConfiguration();
-        config.setSymbolResolver(symbolSolver);
-
+        ParserConfiguration config = SolveThatSymbolSolver.GetConfig(sourcePath);
         Path pathToSource = Paths.get(sourcePath);
         SourceRoot sourceRoot = new SourceRoot(pathToSource, config);
 
