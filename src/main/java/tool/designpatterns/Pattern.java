d59f9919556c4247cd6a0d7505b7f6a7e57f0cb2
MVacante <vacante@student.chalmers.se>
2020-04-01 15:31:08 +0200

    Added a VerifyerUtil class, extracted methods into VerifyerUtil, updated StaticAnalyser.xml to new line length (100)

--- src/main/java/tool/designpatterns/Pattern.java
+++ src/main/java/tool/designpatterns/Pattern.java
@@ -5,16 +5,15 @@ package tool.designpatterns;
  * new pattern has been added, then the enum must be expanded manually.
  */
 public enum Pattern {
-    
-    IMMUTABLE(PatternGroup.IMMUTABLE), 
-    
+    IMMUTABLE(PatternGroup.IMMUTABLE),
+
     SINGLETON(PatternGroup.SINGLETON),
-    
+
     DECORATOR_INTERFACE_COMPONENT(PatternGroup.DECORATOR),
     DECORATOR_CONCRETE_COMPONENT(PatternGroup.DECORATOR),
     DECORATOR_ABSTRACT_DECORATOR(PatternGroup.DECORATOR),
-    DECORATOR_CONCRETE_DECORATOR(PatternGroup.DECORATOR);
-    
+    DECORATOR_CONCRETE_DECORATOR(PatternGroup.DECORATOR),
+
     ADAPTER_ADAPTER(PatternGroup.ADAPTER),
     ADAPTER_ADAPTEE(PatternGroup.ADAPTER);
 
--- src/main/java/tool/designpatterns/verifiers/multiclassverifiers/decorator/DecoratorVerifier.java
+++ src/main/java/tool/designpatterns/verifiers/multiclassverifiers/decorator/DecoratorVerifier.java
@@ -3,22 +3,14 @@ package tool.designpatterns.verifiers.multiclassverifiers.decorator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.ConstructorDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.InitializerDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.body.VariableDeclarator;
 
 import tool.designpatterns.Pattern;
 import tool.designpatterns.PatternGroup;
 import tool.designpatterns.verifiers.IPatternGrouper;
+import tool.designpatterns.verifiers.VerifierUtils;
 import tool.feedback.Feedback;
-import tool.feedback.FeedbackTrace;
 import tool.feedback.PatternGroupFeedback;
 
 /**
@@ -47,7 +39,8 @@ public class DecoratorVerifier implements IPatternGrouper {
     }
 
     /**
-     * <p>Verifies that a correct implementation of the decorator interface has been found.</p>
+     * <p>Verifies that a correct implementation of the decorator interface has
+     * been found.</p>
      * <p>Any valid implementation must fulfill the following requirements:
      *      <ol>
      *          <li>Any given pattern instance must contain:
@@ -58,20 +51,25 @@ public class DecoratorVerifier implements IPatternGrouper {
      *             <li>At least one concrete decorator</li>
      *              </ul>
      *          </li>
-     *          <li>The component interface must contain at least one method</li>
+     *          <li>The component interface must contain at least one
+     *          method</li>
      *
      *         <li>Every abstract decorator must fulfill the following:
      *             <ul>
      *                  <li>It must house a component field</li>
-     *                  <li>The component field needs to be initialized during construction</li>
+     *                  <li>The component field needs to be initialized
+     *                  during construction</li>
      *             </ul>
      *         </li>
      *         <li>
-     *             Every concrete decorator must extend an existing abstract decorator in the
-     *             current the pattern instance (implicitly done when calling getPatternInstances())
+     *             Every concrete decorator must extend an existing abstract
+     *             decorator in the
+     *             current the pattern instance (implicitly done when calling
+     *             getPatternInstances())
      *         </li>
      *         <li>
-     *             Every concrete component must extend the interface component (implicitly done
+     *             Every concrete component must extend the interface
+     *             component (implicitly done
      *             when calling getPatternInstances())
      *         </li>
      *      </ol>
@@ -80,7 +78,7 @@ public class DecoratorVerifier implements IPatternGrouper {
      * @param patternInstance An instance of the decorator pattern to be verified
      *
      * @return A {@link Feedback} object that contains the result and information regarding whether
-     *     ther or not the instance of the pattern was valid
+     *     ther ther or not the instance of the pattern was valid
      */
     @SuppressWarnings("PMD.LinguisticNaming")
     private Feedback verify(DecoratorPatternInstance patternInstance) {
@@ -94,131 +92,19 @@ public class DecoratorVerifier implements IPatternGrouper {
         Feedback hasAllElements = patternInstance.hasAllElements();
         childFeedbacks.add(hasAllElements);
         if (!hasAllElements.getIsError()) {
-            childFeedbacks.add(interfaceContainsMethod(interfaceComponent));
+            childFeedbacks.add(VerifierUtils.hasAtLeastOnePublicMethod(interfaceComponent));
             abstractDecorators.forEach(decorator -> {
-                childFeedbacks.add(hasFieldOfType(decorator, interfaceComponent));
-                childFeedbacks.add(fieldInitializedInConstr(decorator, interfaceComponent));
+                childFeedbacks.add(VerifierUtils.hasFieldOfType(decorator, interfaceComponent));
+                childFeedbacks.add(
+                    VerifierUtils.initializesTypeInAllConstructors(decorator, interfaceComponent));
             });
             concreteDecorators.forEach(decorator -> {
-                childFeedbacks.add(fieldInitializedInConstr(decorator, interfaceComponent));
+                childFeedbacks.add(
+                    VerifierUtils.initializesTypeInAllConstructors(decorator, interfaceComponent));
             });
         }
 
         return Feedback.getPatternInstanceFeedback(childFeedbacks);
     }
-
-    /**
-     * Checks that an interfaceComponent contains at least one public method.
-     *
-     * @param interfaceComponent The interface of the interfaceComponent
-     *
-     * @return A {@link Feedback} object containing the result
-     */
-    public Feedback interfaceContainsMethod(
-        ClassOrInterfaceDeclaration interfaceComponent) {
-        Feedback result;
-
-        AtomicBoolean resultBool = new AtomicBoolean(false);
-        for (MethodDeclaration methodDeclaration : interfaceComponent.findAll(
-            MethodDeclaration.class)) {
-            if (!methodDeclaration.isPrivate()) {
-                resultBool.set(true);
-            }
-        }
-
-        if (resultBool.get()) {
-            result = Feedback.getSuccessfulFeedback();
-        } else {
-            String msg = "Interface does not contain any methods!";
-            result = Feedback.getPatternInstanceNoChildFeedback(msg);
-        }
-
-        return result;
-    }
-
-    /**
-     * Method for checking that a class contains a field of a certain type.
-     *
-     * @param toTest The class to check
-     * @param type   The class or interface that toTest should contain
-     *
-     * @return A {@link Feedback} object containing the result
-     */
-    @SuppressWarnings("PMD.LinguisticNaming")
-    private Feedback hasFieldOfType(
-        ClassOrInterfaceDeclaration toTest, ClassOrInterfaceDeclaration type) {
-        Feedback result;
-        AtomicBoolean hasAComponent = new AtomicBoolean(false);
-        toTest.findAll(VariableDeclarator.class).forEach(fieldDeclaration -> {
-            if (fieldDeclaration.getTypeAsString().equals(
-                type.getNameAsString())) { //Check that this works
-                hasAComponent.set(true);
-            }
-        });
-        if (hasAComponent.get()) {
-            result = Feedback.getSuccessfulFeedback();
-        } else {
-            result = Feedback.getNoChildFeedback(
-                "There was no field with type '" + type.getNameAsString() + "'",
-                new FeedbackTrace(toTest));
-        }
-        return result;
-    }
-
-    /**
-     * Checks if all constructors in a given class initialize the Component field in the class.
-     *
-     * @param toTest        The class to check.
-     * @param interfaceName
-     *
-     * @return A {@link Feedback} object containing the result
-     */
-    private Feedback fieldInitializedInConstr(
-        ClassOrInterfaceDeclaration toTest, ClassOrInterfaceDeclaration interfaceName) {
-        Feedback result;
-        AtomicBoolean isInitialized = new AtomicBoolean(true);
-        List<FieldDeclaration> fieldsInClass = toTest.findAll(FieldDeclaration.class);
-        String nameOfInterface = interfaceName.getNameAsString();
-        List<String> constructorParams = new ArrayList<>();
-        for (FieldDeclaration currentField : fieldsInClass) {
-            if (currentField.getCommonType().toString().equals(nameOfInterface)) {
-                isInitialized.compareAndSet(true, !currentField.isPublic());
-                if (isInitialized.get()) {
-                    currentField.findAll(InitializerDeclaration.class).forEach(fieldInitializer -> {
-                        isInitialized.set(false);
-                    });
-                    toTest.findAll(ConstructorDeclaration.class).forEach(declaration -> {
-                        Optional<Parameter> parameter = declaration.getParameterByType(
-                            nameOfInterface);
-                        if (parameter.isPresent()) {
-                            constructorParams.add(parameter.get().getNameAsString());
-                        } else {
-                            isInitialized.set(false);
-                        }
-                    });
-                    toTest.findAll(VariableDeclarator.class).forEach(variableDeclarator -> {
-                        if (variableDeclarator.getNameAsString().equals(
-                            currentField.getVariable(0).getNameAsString()) &&
-                            variableDeclarator.getInitializer().isPresent() &&
-                            !constructorParams.contains(
-                                variableDeclarator.getInitializer().get().toString())) {
-                            isInitialized.set(false);
-
-                        }
-                    });
-                }
-            }
-            constructorParams.clear();
-        }
-        if (isInitialized.get()) {
-            result = Feedback.getSuccessfulFeedback();
-        } else {
-            result = Feedback.getNoChildFeedback(
-                "All constructors did not initialize the Component field",
-                new FeedbackTrace(toTest));
-        }
-        return result;
-    }
-
 }
 
