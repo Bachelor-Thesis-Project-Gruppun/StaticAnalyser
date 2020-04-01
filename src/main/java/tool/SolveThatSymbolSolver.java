5dc28c798fc66b77457a80f079601a10ba8c1c06
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-01 14:14:40 +0200

    No longer creates duplicate package roots.

--- src/main/java/tool/SolveThatSymbolSolver.java
+++ src/main/java/tool/SolveThatSymbolSolver.java
@@ -2,7 +2,6 @@ package tool;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -39,11 +38,19 @@ public final class SolveThatSymbolSolver {
             new JavaParserTypeSolver(lookForTypes), new ReflectionTypeSolver(),
             new JavaParserTypeSolver(new File("src/test/java")));
 
-        List<JavaParserTypeSolver> typeSolvers = null;
+        List<File> packageRoots = null;
         try {
-            typeSolvers = getSourceTypeSolvers(new File(rootDir));
-            for (JavaParserTypeSolver solver : typeSolvers) {
-                typeSolver.add(solver);
+            packageRoots = getPackageRoots(new File(rootDir));
+            List<File> noDuplicates = new ArrayList<>();
+            for (File root : packageRoots) {
+                if (!noDuplicates.contains(root)) {
+                    noDuplicates.add(root);
+                }
+            }
+
+            for (File pkgRoot : noDuplicates) {
+                System.out.println("Adding package root" + pkgRoot.toString());
+                typeSolver.add(new JavaParserTypeSolver(pkgRoot));
             }
         } catch (IllegalArgumentException e) {
             System.err.println("Unable to get config for " + rootDir);
@@ -59,44 +66,78 @@ public final class SolveThatSymbolSolver {
 
     /**
      * Goes through and tries to find each ''package root'' in the subdirectories of the given dir.
+     * May contain duplicates!
      *
      * @param rootDir the root dir.
      *
-     * @return A list of JavaParserTypeSolvers, one for each package root found. Alternatively an
-     *     empty list if none could be found.
+     * @return A list of Files, one for each package root found. Alternatively an empty list if none
+     *     could be found.
      */
-    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops") // Would be less performant to
-    // cache nodes in a list and then creating TypeSolvers from them.
-    private static List<JavaParserTypeSolver> getSourceTypeSolvers(File rootDir)
-        throws IllegalArgumentException {
-        List<JavaParserTypeSolver> typeSolvers = new ArrayList<>();
+    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+    // Would be less performant to cache nodes in a list and then creating TypeSolvers from
+    // them (regarding the AvoidInstantiatingObjectsInLoops).
+    private static List<File> getPackageRoots(File rootDir) throws IllegalArgumentException {
+        List<File> packageRoots = new ArrayList<>();
 
         // It is a directory, let's keep going
         File[] childFiles = rootDir.listFiles();
+        if (childFiles == null) {
+            return packageRoots;
+        }
+
         for (File child : childFiles) {
             if (child.getName().endsWith(".java")) {
                 // Find the root package for this java file.
-                String pkg = getPackage(child);
-
-                int numFolders = pkg.isEmpty() ? 0 : getCharOccurances(pkg, '.') + 1;
-                File node = child;
-                while (numFolders > 0) {
-                    numFolders--;
-                    node = node.getParentFile();
-                    if (node == null) {
-                        throw new IllegalArgumentException(
-                            "Unable to find root package directory for " + rootDir.toString());
-                    }
-
-                    typeSolvers.add(new JavaParserTypeSolver(node));
+                String pkg = "";
+                try {
+                    pkg = getPackage(child);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
 
+                File node = getPackageRootNode(pkg, child);
+                packageRoots.add(node);
             } else if (child.isDirectory()) {
-                typeSolvers.addAll(getSourceTypeSolvers(child));
+                packageRoots.addAll(getPackageRoots(child));
             }
         }
 
-        return typeSolvers;
+        return packageRoots;
+    }
+
+    private static List<File> addIfNotDuplicate(List<File> base, List<File> toAdd) {
+        List<File> newList = new ArrayList<>();
+        newList.addAll(base);
+
+        for (File file : toAdd) {
+            boolean exists = false;
+            for (File existing : base) {
+                if (file.getAbsolutePath().equals(existing.getAbsolutePath())) {
+                    exists = true;
+                }
+            }
+
+            if (!exists) {
+                newList.add(file);
+            }
+        }
+
+        return newList;
+    }
+
+    private static File getPackageRootNode(String pkg, File base) {
+        int numFolders = pkg.isEmpty() ? 0 : getCharOccurances(pkg, '.') + 1;
+
+        File node = base;
+        while (numFolders > 0) {
+            numFolders--;
+            node = node.getParentFile();
+            if (node == null) {
+                throw new IllegalArgumentException(
+                    "Unable to find root package directory for " + base.toString());
+            }
+        }
+        return node;
     }
 
     /**
@@ -124,49 +165,36 @@ public final class SolveThatSymbolSolver {
      *
      * @return the package or an empty string if not found.
      */
-    private static String getPackage(File file) {
+    @SuppressWarnings("PMD.CloseResource")
+    private static String getPackage(File file) throws IOException {
         StringBuilder pkg = new StringBuilder();
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = Files.newBufferedReader(file.toPath());
-            String line = bufferedReader.readLine();
+        BufferedReader bufferedReader = Files.newBufferedReader(file.toPath());
+        String line = bufferedReader.readLine();
 
-            // Used as a package can be declared on multiple lines.
-            boolean foundPackage = false;
-            while (line != null) {
-                String toAppend = line;
-                if (!foundPackage) {
-                    toAppend = getCleanedPackageString(line);
-                    if (toAppend != null) {
-                        foundPackage = true;
-                    }
+        // Used as a package can be declared on multiple lines.
+        boolean foundPackage = false;
+        while (line != null) {
+            String toAppend = line;
+            if (!foundPackage) {
+                toAppend = getCleanedPackageString(line);
+                if (toAppend != null) {
+                    foundPackage = true;
                 }
-
-                if (foundPackage) {
-                    pkg.append(toAppend);
-                    String theString = pkg.toString();
-                    if (theString.contains(";")) {
-                        bufferedReader.close();
-                        // Remove the semi-colon and everything after it and return.
-                        return theString.substring(0, theString.indexOf(";"));
-                    }
-                }
-                line = bufferedReader.readLine();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("No package found for file " + file.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            if (foundPackage) {
+                pkg.append(toAppend);
+                String theString = pkg.toString();
+                if (theString.contains(";")) {
+                    bufferedReader.close();
+                    // Remove the semi-colon and everything after it and return.
+                    return theString.substring(0, theString.indexOf(';'));
+                }
+            }
+            line = bufferedReader.readLine();
         }
 
-        if (bufferedReader != null) {
-            try {
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+        bufferedReader.close();
         return "";
     }
 
@@ -182,8 +210,8 @@ public final class SolveThatSymbolSolver {
         String packageMatch = "package ";
         StringBuilder packageBuilder = new StringBuilder();
         for (int i = 0; i < line.length(); i++) {
-            char c = line.charAt(i);
-            packageBuilder.append(c);
+            char charAtI = line.charAt(i);
+            packageBuilder.append(charAtI);
             if (packageBuilder.toString().equals(packageMatch)) {
                 if (i == line.length() - 1) {
                     // the line ends after the matching string.
