324aa4b6e3f9223288bfec3235108f4453f4a1f9
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-04 16:32:55 +0200

    ClassImplementsMethod now checks if the class implements the interface at all.

--- src/main/java/tool/ProjectParser.java
+++ src/main/java/tool/ProjectParser.java
@@ -1,6 +1,5 @@
 package tool;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- src/main/java/tool/designpatterns/verifiers/multiclassverifiers/proxy/MethodVerification.java
+++ src/main/java/tool/designpatterns/verifiers/multiclassverifiers/proxy/MethodVerification.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.resolution.types.ResolvedReferenceType;
 
 import tool.designpatterns.verifiers.multiclassverifiers.proxy.visitors.MethodDeclarationVisitor;
 import tool.feedback.Feedback;
@@ -13,7 +16,11 @@ import tool.feedback.FeedbackWrapper;
 /**
  * A class containing useful helper methods when verifying methods.
  */
-public class MethodVerification {
+public final class MethodVerification {
+
+    private MethodVerification() {
+
+    }
 
     /**
      * A method to find out if both the interface and the class implement the method.
@@ -27,6 +34,13 @@ public class MethodVerification {
     static FeedbackWrapper<MethodDeclaration> classImplementsMethod(
         ClassOrInterfaceDeclaration theClass, ClassOrInterfaceDeclaration theInterface,
         MethodDeclaration method) {
+
+        // Check if the class implements the interface / abstract class.
+        if (classImplementsInterface(theClass, theInterface)) {
+            return new FeedbackWrapper<>(Feedback.getNoChildFeedback(
+                "Class '" + theClass.getNameAsString() + "' does not implement interface '" +
+                theInterface.getNameAsString() + "'", new FeedbackTrace(theClass)), null);
+        }
 
         MethodDeclarationVisitor mdv = new MethodDeclarationVisitor();
         List<MethodDeclaration> implementedMethods = theClass.accept(mdv, theClass);
@@ -71,5 +85,35 @@ public class MethodVerification {
         String message = "The method is not implemented in the class or the interface";
         return new FeedbackWrapper<>(
             Feedback.getNoChildFeedback(message, new FeedbackTrace(theInterface)), null);
+    }
+
+    /**
+     * Verifies that the given class implements the given interface (or abstract class).
+     *
+     * @param theClass     the class to check.
+     * @param theInterface the interface to check for.
+     *
+     * @return true if the class implements the interface, otherwise false.
+     */
+    private static boolean classImplementsInterface(
+        ClassOrInterfaceDeclaration theClass, ClassOrInterfaceDeclaration theInterface) {
+        ResolvedReferenceTypeDeclaration resolvedInterfaceDec = theInterface.resolve();
+        for (ClassOrInterfaceType interf : theClass.getImplementedTypes()) {
+            ResolvedReferenceType resolvedInterface = interf.resolve();
+            if (resolvedInterface.getTypeDeclaration().equals(resolvedInterfaceDec)) {
+                return true;
+            }
+        }
+
+        // The class does not implement the interface but it could extend it as an abstract class.
+        if (theInterface.isAbstract()) {
+            for (ClassOrInterfaceType extended : theClass.getExtendedTypes()) {
+                if (extended.resolve().getTypeDeclaration().equals(resolvedInterfaceDec)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
