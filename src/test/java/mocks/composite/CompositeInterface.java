8738a47a5f040a8603a2989a0625a3dc5e2a2af8
Eric Carlsson <cirecirecire3@gmail.com>
2020-04-06 12:14:15 +0200

    remove annotations from testclasses

--- src/test/java/mocks/composite/CompositeInterface.java
+++ src/test/java/mocks/composite/CompositeInterface.java
@@ -3,9 +3,6 @@ package mocks.composite;
 import tool.designpatterns.DesignPattern;
 import tool.designpatterns.Pattern;
 
-@DesignPattern(pattern = {
-    Pattern.COMPOSITE_COMPONENT
-})
 public interface CompositeInterface {
 
     void test(int a);
--- src/test/java/mocks/composite/CompositeMock.java
+++ src/test/java/mocks/composite/CompositeMock.java
@@ -6,9 +6,6 @@ import java.util.List;
 import tool.designpatterns.DesignPattern;
 import tool.designpatterns.Pattern;
 
-@DesignPattern(pattern = {
-    Pattern.COMPOSITE_CONTAINER, Pattern.COMPOSITE_LEAF
-})
 public class CompositeMock implements CompositeInterface {
 
     List<CompositeInterface> foo = new ArrayList<>();
