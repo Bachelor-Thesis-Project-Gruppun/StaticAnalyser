dce2ab926bee3fa76ec699962c34d99bbe708526
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-07 17:15:57 +0200

    Reformatted PatternGroup

--- src/main/java/tool/designpatterns/PatternGroup.java
+++ src/main/java/tool/designpatterns/PatternGroup.java
@@ -1,9 +1,8 @@
 package tool.designpatterns;
 
-import tool.designpatterns.verifiers.multiclassverifiers.decorator.DecoratorVerifier;
-import tool.designpatterns.verifiers.multiclassverifiers.AdapterVerifier;
 import tool.designpatterns.verifiers.IPatternGrouper;
 import tool.designpatterns.verifiers.multiclassverifiers.adapter.AdapterVerifier;
+import tool.designpatterns.verifiers.multiclassverifiers.decorator.DecoratorVerifier;
 import tool.designpatterns.verifiers.multiclassverifiers.proxy.ProxyVerifier;
 import tool.designpatterns.verifiers.singleclassverifiers.SingleClassGrouper;
 import tool.designpatterns.verifiers.singleclassverifiers.immutable.ImmutableVerifier;
@@ -13,8 +12,8 @@ import tool.designpatterns.verifiers.singleclassverifiers.singleton.SingletonVer
  * A design pattern covering all parts of that pattern..
  */
 public enum PatternGroup {
-    SINGLETON(new SingleClassGrouper(new SingletonVerifier())), 
-    IMMUTABLE(new SingleClassGrouper(new ImmutableVerifier())), 
+    SINGLETON(new SingleClassGrouper(new SingletonVerifier())),
+    IMMUTABLE(new SingleClassGrouper(new ImmutableVerifier())),
     DECORATOR(new DecoratorVerifier()),
     PROXY(new ProxyVerifier()),
     ADAPTER(new AdapterVerifier());
