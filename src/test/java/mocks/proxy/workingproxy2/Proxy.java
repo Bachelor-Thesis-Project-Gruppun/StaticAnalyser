d77ee5f865a7b46062db687bfad25076d23c5a93
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-07 14:04:46 +0200

    Add new testcase for proxy pattern.

---
+++ src/test/java/mocks/proxy/workingproxy2/Proxy.java
@@ -0,0 +1,25 @@
+package mocks.proxy.workingproxy2;
+
+import tool.designpatterns.DesignPattern;
+import tool.designpatterns.Pattern;
+
+@DesignPattern(pattern = {Pattern.PROXY_PROXY})
+public class Proxy implements ProxyInterface {
+
+    private Subject theSubject;
+
+    public Proxy(Subject theSubject) {
+        this.theSubject = theSubject;
+    }
+
+    @Override
+    public int[] getTextIDs() {
+        this.theSubject.getText(0);
+        return this.theSubject.getTextIDs();
+    }
+
+    @Override
+    public String getText(int id) {
+        return theSubject.getText(id);
+    }
+}
