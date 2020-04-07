133761a52cb78645e6675de47d660cc7d8ed42f4
Vidar Magnusson <vidar.halmstad@hotmail.com>
2020-04-07 17:15:45 +0200

    Readded decorator

--- src/main/java/tool/designpatterns/Pattern.java
+++ src/main/java/tool/designpatterns/Pattern.java
@@ -1,8 +1,8 @@
 package tool.designpatterns;
 
 /**
- * An enum that contains all patterns we are able to identify and verify. If a
- * new pattern has been added, then the enum must be expanded manually.
+ * An enum that contains all patterns we are able to identify and verify. If a new pattern has been
+ * added, then the enum must be expanded manually.
  */
 public enum Pattern {
     // Single class patterns
@@ -14,6 +14,12 @@ public enum Pattern {
     PROXY_INTERFACE(PatternGroup.PROXY),
     PROXY_SUBJECT(PatternGroup.PROXY),
     PROXY_PROXY(PatternGroup.PROXY),
+
+    // Decorator
+    DECORATOR_INTERFACE_COMPONENT(PatternGroup.DECORATOR),
+    DECORATOR_CONCRETE_COMPONENT(PatternGroup.DECORATOR),
+    DECORATOR_ABSTRACT_DECORATOR(PatternGroup.DECORATOR),
+    DECORATOR_CONCRETE_DECORATOR(PatternGroup.DECORATOR),
 
     // Adapter
     ADAPTER_ADAPTER(PatternGroup.ADAPTER),
