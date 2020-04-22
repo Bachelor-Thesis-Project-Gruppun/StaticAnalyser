d4bff8c4201a5dc7723ef159bed66bfe0774cde8
Li <li.ronning@gmail.com>
2020-04-22 15:29:27 +0200

    add broken proxy mock that does not implement an interface

---
+++ src/test/java/mocks/proxy/proxynoimplementinterface/Proxy.java
@@ -0,0 +1,22 @@
+package mocks.proxy.proxynoimplementinterface;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings(justification = "Mock")
+public class Proxy {
+
+    private Subject theSubject;
+
+    public Proxy(Subject theSubject) {
+        this.theSubject = theSubject;
+    }
+
+    public int[] getTextIDs() {
+        this.theSubject.getText(0);
+        return this.theSubject.getTextIDs();
+    }
+
+    public String getText(int id) {
+        return theSubject.getText(id);
+    }
+}
---
+++ src/test/java/mocks/proxy/proxynoimplementinterface/ProxyInterface.java
@@ -0,0 +1,8 @@
+package mocks.proxy.proxynoimplementinterface;
+
+public interface ProxyInterface {
+
+    int[] getTextIDs();
+
+    String getText(int id);
+}
